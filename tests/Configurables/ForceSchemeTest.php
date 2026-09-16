270e7b241b495a4df7fffb9f8e3e7231c50b7022
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2026-09-16 16:07:39 -0300

    Formatting

--- tests/Configurables/ForceSchemeTest.php
+++ tests/Configurables/ForceSchemeTest.php
@@ -44,29 +44,65 @@ class ForceSchemeTest extends TestCase
                 $this->forcedScheme = $scheme;
             }
 
-            public function current() {}
+            public function current()
+            {
+                //
+            }
 
-            public function previous($fallback = false) {}
+            public function previous($fallback = false)
+            {
+                //
+            }
 
-            public function to($path, $extra = [], $secure = null) {}
+            public function to($path, $extra = [], $secure = null)
+            {
+                //
+            }
 
-            public function secure($path, $parameters = []) {}
+            public function secure($path, $parameters = [])
+            {
+                //
+            }
 
-            public function asset($path, $secure = null) {}
+            public function asset($path, $secure = null)
+            {
+                //
+            }
 
-            public function route($name, $parameters = [], $absolute = true) {}
+            public function route($name, $parameters = [], $absolute = true)
+            {
+                //
+            }
 
-            public function signedRoute($name, $parameters = [], $expiration = null, $absolute = true) {}
+            public function signedRoute($name, $parameters = [], $expiration = null, $absolute = true)
+            {
+                //
+            }
 
-            public function temporarySignedRoute($name, $expiration, $parameters = [], $absolute = true) {}
+            public function temporarySignedRoute($name, $expiration, $parameters = [], $absolute = true)
+            {
+                //
+            }
 
-            public function query($path, $query = [], $extra = [], $secure = null) {}
+            public function query($path, $query = [], $extra = [], $secure = null)
+            {
+                //
+            }
 
-            public function action($action, $parameters = [], $absolute = true) {}
+            public function action($action, $parameters = [], $absolute = true)
+            {
+                //
+            }
 
-            public function getRootControllerNamespace() {}
+            public function getRootControllerNamespace()
+            {
+                //
+            }
 
-            public function setRootControllerNamespace($rootNamespace) {}
+            public function setRootControllerNamespace($rootNamespace)
+            {
+                //
+            }
         };
 
         URL::swap($urlGenerator);
